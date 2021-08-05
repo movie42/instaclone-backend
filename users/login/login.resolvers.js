340597a7bb807b592d83a8de99547e2a5bf385bf
ko Hyunsu <movie3342@gmail.com>
2021-08-05 14:02:24 +0900

    devided resolvers and typeDefs

--- users/login/login.resolvers.js
+++ users/login/login.resolvers.js
@@ -1,27 +1,9 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import client from "../client";
+import client from "../../client";
 
 export default {
   Mutation: {
-    createAccount: async (_, { name, userName, email, password }) => {
-      try {
-        const existingUser = await client.user.findFirst({
-          where: {
-            OR: [{ userName }, { email }],
-          },
-        });
-        if (existingUser) {
-          throw new Error("사용자이름 혹은 이메일이 이미 존재합니다.");
-        }
-        const hashed = await bcrypt.hash(password, 10);
-        return client.user.create({
-          data: { userName, email, name, password: hashed },
-        });
-      } catch (error) {
-        return error;
-      }
-    },
     login: async (_, { userName, password }) => {
       const user = await client.user.findFirst({ where: { userName } });
       if (!user) {
--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -9,22 +9,4 @@ export default gql`
     createdAt: String!
     updatedAt: String!
   }
-  type LoginResult {
-    ok: Boolean!
-    token: String
-    error: String
-  }
-
-  type Mutation {
-    createAccount(
-      name: String!
-      userName: String!
-      email: String!
-      password: String!
-    ): User
-    login(userName: String!, password: String!): LoginResult!
-  }
-  type Query {
-    seeProfile(userName: String!): User
-  }
 `;
