74fe69924b3a4e7053bf1fd00d40153a5e2866be
ko Hyunsu <movie3342@gmail.com>
2022-02-16 13:45:25 +0900

    user utils and resolveFunction

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
+import { getUser } from "./users/users.utils";
 
 const server = new ApolloServer({
   schema,
-  context: {
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjQ0OTA2Mzc2fQ.FfDYgrBZHDHKZZi55YJShpF9d8sSr-9U2ROFWbCBJ7c",
+  context: async ({ req }) => {
+    return {
+      loggedInUser: await getUser(req.headers.token),
+    };
   },
 });
 
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,41 +1,42 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import client from "../../client";
+import { protectedResolver } from "../users.utils";
 
 export default {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, userName, email, password },
-      { token },
-    ) => {
-      const { id } = await jwt.verify(token, process.env.JWT_SECRET_KEY);
-      let hashPassword = null;
-      if (password) {
-        hashPassword = await bcrypt.hash(password, 10);
-      }
-
-      const updateUser = await client.user.update({
-        where: {
-          id,
-        },
-        data: {
-          firstName,
-          lastName,
-          userName,
-          email,
-          ...(hashPassword && { password: hashPassword }),
-        },
-      });
-
-      if (updateUser.id) {
-        return { ok: true };
-      } else {
-        return {
-          ok: false,
-          error: "사용자 정보를 바꿀 수 없습니다.",
-        };
-      }
-    },
+    editProfile: protectedResolver(
+      async (
+        _,
+        { firstName, lastName, username, email, password: newPassword },
+        { loggedInUser },
+      ) => {
+        let hashPassword = null;
+        if (newPassword) {
+          hashPassword = await bcrypt.hash(newPassword, 10);
+        }
+        const updatedUser = await client.user.update({
+          where: {
+            id: loggedInUser.id,
+          },
+          data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            ...(hashPassword && { password: hashPassword }),
+          },
+        });
+        if (updatedUser.id) {
+          return {
+            ok: true,
+          };
+        } else {
+          return {
+            ok: false,
+            error: "Could not update profile.",
+          };
+        }
+      },
+    ),
   },
 };
