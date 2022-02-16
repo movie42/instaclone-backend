91a35a2e028d2e50fb2ef8fdbf7b8da720740863
ko Hyunsu <movie3342@gmail.com>
2022-02-16 16:06:46 +0900

    photo upload with file system in nodejs install express and apollo-server-express 사진 업로드를 uploads폴더에 하고 url을 웹에서 사용할 수 있도록 하게한다.

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 import { ApolloServer } from "apollo-server-express";
 import { graphqlUploadExpress } from "graphql-upload";
 import express from "express";
+import morgan from "morgan";
 import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
 
 const PORT = process.env.PORT;
-
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
@@ -19,12 +19,16 @@ const startServer = async () => {
   });
 
   await server.start();
-
   const app = express();
+  app.use(morgan("tiny"));
+  app.use(express.static("uploads"));
   app.use(graphqlUploadExpress());
   server.applyMiddleware({ app });
-  await new Promise((func) => app.listen({ port: PORT }, func));
-  console.log(`🚀 Server: http://localhost:${PORT}${server.graphqlPath}`);
+
+  app.listen(
+    { port: PORT },
+    console.log(`🚀 Server: http://localhost:${PORT}${server.graphqlPath}`),
+  );
 };
 
 startServer();
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import bcrypt from "bcrypt";
 import client from "../../client";
 import { protectedResolver } from "../users.utils";
@@ -5,9 +6,23 @@ import { GraphQLUpload } from "graphql-upload";
 
 const resolvers = async (
   _,
-  { firstName, lastName, username, email, password: newPassword, bio },
+  { firstName, lastName, username, email, password: newPassword, bio, avatar },
   { loggedInUser },
 ) => {
+  let avatarUrl = null;
+  if (avatar) {
+    const {
+      file: { filename, createReadStream },
+    } = await avatar;
+    const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
+    const readStream = createReadStream();
+    const writeStream = fs.createWriteStream(
+      process.cwd() + "/uploads/" + newFilename,
+    );
+    readStream.pipe(writeStream);
+    avatarUrl = `http://localhost:3000/${newFilename}`;
+  }
+
   let hashPassword = null;
   if (newPassword) {
     hashPassword = await bcrypt.hash(newPassword, 10);
@@ -22,6 +37,7 @@ const resolvers = async (
       username,
       email,
       bio,
+      ...(avatarUrl && { avatar: avatarUrl }),
       ...(hashPassword && { password: hashPassword }),
     },
   });
